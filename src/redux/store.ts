802a4240ed5e346c659a98adcba710f4f951ba55
Arko <arkoroyb@gmail.com>
2023-07-07 00:25:06 +0600

    redux for cart done

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit'
-
+import cartReducer from './features/cart/cartSlice'
 
 const store = configureStore({
     reducer: {
-
+        cart: cartReducer
     }
 })
 
